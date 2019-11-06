dfd2dd82aae30bb06b6e56e08a0e07a4f413e79e
Nehal <neha;l.hdr@gmail.com>
2019-11-06 16:05:45 +0530

    new file added

---
+++ app/src/main/java/dev/nehal/insane/newd/main/ViewDownloadsActivity.kt
@@ -0,0 +1,15 @@
+package dev.nehal.insane.newd.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dev.nehal.insane.R
+
+class ViewDownloadsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_downloads)
+
+
+    }
+}
